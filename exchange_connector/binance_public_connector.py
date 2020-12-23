8d68e793c7da38afb79b95be8709c9141760b108
qqdog1 <qqdog1@yahoo.com.tw>
2020-12-23 22:52:19 +0800

    1. subscribe order book 2. parse json data 3. 但是收到資料沒有symbol根本不知道是誰的

--- exchange_connector/binance_public_connector.py
+++ exchange_connector/binance_public_connector.py
@@ -1,16 +1,22 @@
 import abc
+import json
 import threading
 import time
 
 import websocket
 
 
+def callback_test(symbol:str, bid_price: float, bid_qty: float, ask_price: float, ask_qty: float):
+    print(symbol, ':', bid_price, '@', bid_qty, " : ", ask_price, '@', ask_qty)
+
+
 class BinancePublicConnector(metaclass=abc.ABCMeta):
     def __init__(self):
-        self.connect = False
+        self.connect_status = False
+        self.callback_dict = {}
         websocket.enableTrace(True)
         self.ws = websocket.WebSocketApp(
-            # "wss://stream.binance.com:9443/ws/btcusdt@depth",
+            # "wss://stream.binance.com:9443/stream?streams=" + topics,
             "wss://stream.binance.com:9443/ws",
             on_message=self.on_message,
             on_error=self.on_error,
@@ -20,31 +26,42 @@ class BinancePublicConnector(metaclass=abc.ABCMeta):
 
     @property
     def is_connect(self):
-        return self.connect
+        return self.connect_status
 
     def start(self):
         self.ws.run_forever()
 
     def subscribe(self, market: str, callback):
-        pass
+        symbol = market.replace('-', '').lower()
+        self.callback_dict[symbol] = callback
+        message = '{"method":"SUBSCRIBE","params":["' + symbol + '@depth5"],"id":1}'
+        self.ws.send(message)
 
-    def unsubscribe(self):
-        pass
+    def unsubscribe(self, market: str):
+        symbol = market.replace('-', '').lower()
+        if symbol in self.callback_dict.keys():
+            self.callback_dict.pop(symbol)
 
     def on_open(self):
         print("onOpen")
-        self.connect = True
+        self.connect_status = True
 
     def on_message(self, message):
-        print(message)
+        json_node = json.loads(message)
+        if 'lastUpdateId' in json_node:
+            self.parse_book(json_node)
 
     def on_error(self):
         print("onerror")
-        self.connect = False
+        self.connect_status = False
 
     def on_close(self):
         print("onclose")
-        self.connect = False
+        self.connect_status = False
+
+    def parse_book(self, json_node):
+        print(json_node)
+        print(json_node['bids'][0][0])
 
 
 if __name__ == '__main__':
@@ -53,7 +70,7 @@ if __name__ == '__main__':
     t1.start()
 
     while not connector.is_connect:
-        print("connect to websocket :", connector.is_connect)
         time.sleep(0.1)
 
-    print("websocket connected")
+    connector.subscribe('BTC-USDT', callback_test)
+    connector.subscribe('ETH-USDT', callback_test)
