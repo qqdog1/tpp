6501031ada7b1dc213f3fcfa0812f41b26a6fc0d
Shawn <qqdog1@yahoo.com.tw>
2020-12-23 14:25:05 +0800

    1. use pipenv

--- exchange_connector/max_public_connector.py
+++ exchange_connector/max_public_connector.py
@@ -1,34 +1,7 @@
 import abc
-from websocket import WebSocketApp
-
-
-class MessageFunc(object):
-    pass
-
-
-class ErrorFunc(object):
-    pass
-
-
-class CloseFunc(object):
-    pass
-
-
-class OpenFunc(object):
-    print(object)
 
 
 class MaxPublicConnector(metaclass=abc.ABCMeta):
-    def __init__(self):
-        ws = WebSocketApp(
-            'wss://max-stream.maicoin.com/ws',
-            on_message=MessageFunc,
-            on_error=ErrorFunc,
-            on_close=CloseFunc
-        )
-        ws.on_open = OpenFunc
-        ws.run_forever()
-
     def subscribe(self):
         pass
 
@@ -37,4 +10,4 @@ class MaxPublicConnector(metaclass=abc.ABCMeta):
 
 
 if __name__ == '__main__':
-    connector = MaxPublicConnector()
+    asyncio.get_event_loop().run_until_complete(hello())
